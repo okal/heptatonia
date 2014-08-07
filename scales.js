50e4cdcd586fd282fc37784075f7ae8de0c2a6c6
Okal Otieno <okal@justokal.com>
2014-08-07 20:32:58 +0300

    Add a function to generate a chromatic scale in a given key

--- scales.js
+++ scales.js
@@ -19,4 +19,15 @@ Patterns.Locrian = [0.5, 1, 1, 0.5, 1, 1, 1]
 Patterns.Major = Patterns.Ionian
 Patterns.Minor = Patterns.Aeolian
 
+var generateChromaticScale = function(key) {
+	var chromaticScale = []
+	var startIndex = notes.getStartPosition(key)
+	var range = _.range(startIndex, notes.size + startIndex)
+	_.each(range, function(index) {
+		chromaticScale.push(notes.getAt(index).value)
+	})
+	return chromaticScale
+}
+
+console.log(generateChromaticScale('B'))
 exports.notes = notes
