a6cc8921e5951d6fa51abd4613ad7edee303f541
Okal Otieno <okal@justokal.com>
2014-08-07 19:55:18 +0300

    Require scales.js in main.js

--- main.js
+++ main.js
@@ -1,10 +1,2 @@
-var _ = require('underscore-node')
-var dll = require('./doublyLinkedList.js')
-
-var chromaticScale = new dll.DoublyLinkedList()
-
-_.each(['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'], function(value) {
-	chromaticScale.push(value)
-})
-
-console.log(chromaticScale)
+var scales = require('./scales.js')
+console.log(scales.notes)
