dcf4f032202be0de415885737d38780c695b9073
Okal Otieno <okal@justokal.com>
2014-08-07 20:31:28 +0300

    Add mode patterns

--- scales.js
+++ scales.js
@@ -7,4 +7,16 @@ _.each(['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'], functi
 	notes.push(value)
 })
 
+var Patterns = {}
+Patterns.Ionian = [1, 1, 0.5, 1, 1, 1, 0.5]
+Patterns.Dorian =  [1, 0.5, 1, 1, 1, 0.5, 1]
+Patterns.Phrygian =  [0.5, 1, 1, 1, 0.5, 1, 1]
+Patterns.Lydian = [1, 1, 1, 0.5, 1, 1, 0.5]
+Patterns.Mixolydian = [1, 1, 0.5, 1, 1, 0.5, 1]
+Patterns.Aeolian = [1, 0.5, 1, 1, 0.5, 1, 1]
+Patterns.Locrian = [0.5, 1, 1, 0.5, 1, 1, 1]
+
+Patterns.Major = Patterns.Ionian
+Patterns.Minor = Patterns.Aeolian
+
 exports.notes = notes
