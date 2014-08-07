ce515c293012386e49e4f0c649b38b29915451bb
Okal Otieno <okal@justokal.com>
2014-08-07 19:21:08 +0300

    Finalize getAt method

---
+++ doublyLinkedList.js
@@ -0,0 +1,36 @@
+var ListNode = function(value) {
+	this.value = value
+}
+
+var DoublyLinkedList = function() {
+	this.head = undefined
+	this.tail = undefined
+	this.size = 0
+	this.push = function(value) {
+		var listNode = new ListNode(value)
+		if(this.size == 0) {
+			this.head = listNode
+			this.tail = listNode
+		}
+		this.tail.next = listNode
+		listNode.prev = this.tail
+		listNode.next = this.head
+		this.tail = listNode
+		this.head.prev = this.tail
+		this.size++
+	}
+	this.getAt = function(index) {
+		var node = this.head
+		var normalizedIndex = index % this.size
+		while(normalizedIndex < 0) {
+			normalizedIndex += this.size
+		}
+		for(var i = 0; i <= normalizedIndex; i++) {
+			if((i % this.size) == normalizedIndex) {
+				return node
+			} else {
+				node = node.next
+			}
+		}
+	}
+}
